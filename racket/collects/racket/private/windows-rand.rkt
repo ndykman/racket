33eb5e12c8c0f4ee9f822c54e21e8a8812a5cba0
Nathan Dykman <ndykman@gmail.com>
2026-07-03 16:38:52 -0700

    Updated crypto-random-windows-bytes to use bcrypt.dll

--- racket/collects/racket/private/windows-rand.rkt
+++ racket/collects/racket/private/windows-rand.rkt
@@ -4,16 +4,16 @@
 (require ffi/unsafe
          ffi/unsafe/define)
 
-(define-ffi-definer define-bcrypt (and (eq? (system-type) 'windows) (ffi-lib "bcrypt.dll"))
+(define-ffi-definer define-bcryptapi (and (eq? (system-type) 'windows) (ffi-lib "Bcrypt.dll"))
   #:default-make-fail make-not-available)
 
-; Use the bcrypt version 
-(define-bcrypt BCryptGenRandom (_fun _pointer _pointer _ulong _ulong -> _int))
+; Updated to use modern API.
+(define-bcryptapi BCryptGenRandom (_fun _pointer _pointer _ulong _ulong -> _ulong))
 
 ; (: crypto-random-windows-bytes (-> Positive-Integer Bytes))
 (define (crypto-random-windows-bytes n)
   (define rand-bytes-buf (make-bytes n))
-  (if (= (BCryptGenRandom #f rand-bytes-buf n 2) 0)
+  (if (BCryptGenRandom #f rand-bytes-buf n #x02)
       rand-bytes-buf
       (raise (make-exn:fail
               "crypto-random-windows: BCryptGenRandom failed to generate bytes"
